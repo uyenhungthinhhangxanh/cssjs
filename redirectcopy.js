eb5fa7b531810a4cefe1f57e21691d3d63c702ba
uyenhungthinhhangxanh <leuyenms80@gmail.com>
2017-07-01 14:36:23 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://nhadathungthinh24h.net'>http://nhadathungthinh24h.net"+document.location.pathname+"</a>";
+    
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
